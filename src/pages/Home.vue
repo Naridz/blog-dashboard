a50c22903323edc977b6cd638c0f346b501b02de
Naridz <naris2001@gmail.com>
2025-11-10 23:26:30 +0700

    .

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container mx-auto ">
-        <h1 class="text-center m-5 p-5 text-5xl font-bold">Blog Management Dashboard</h1>
+        <h1 class="text-center m-5 p-5 text-5xl font-bold">Blog Management</h1>
         <p class="text-xl text-center text-gray-600 mb-8">Vue. js + Tailwind CSS</p>
     </div>
 </template>
